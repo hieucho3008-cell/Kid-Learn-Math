979c4a233edd1121ad2a96fd9bf9357d5705a5e4
hieucho3008-cell <hieucho3008@gmail.com>
2026-06-13 06:18:48 +0700

    Update finger_counter.py

--- src/finger_counter.py
+++ src/finger_counter.py
@@ -1,1 +1,17 @@
+def count_fingers(hand):
 
+    tips = [4, 8, 12, 16, 20]
+
+    count = 0
+
+    # Thumb
+    if hand[tips[0]][0] > hand[tips[0]-1][0]:
+        count += 1
+
+    # Index -> Pinky
+    for tip in tips[1:]:
+
+        if hand[tip][1] < hand[tip-2][1]:
+            count += 1
+
+    return count
